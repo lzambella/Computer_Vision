e5ba4c5c0cc138dd79d2b5d6af4de94c6be9b44e
Luke Zambella <Zambell1@tcnj.edu>
2019-02-27 14:42:58 -0500

    Refactor reusable code into own functions.

--- board.py
+++ board.py
@@ -4,6 +4,7 @@ This class is preliminary experimentation before tidying things up into their ow
 
 import cv2 as cv
 import numpy as np
+from time import sleep
 
 
 def intersection(x1, y1, x2, y2, x3, y3, x4, y4):
@@ -32,6 +33,7 @@ def intersection(x1, y1, x2, y2, x3, y3, x4, y4):
 
     return (Px, Py), (t,u)
 
+
 def draw_lines(src, lines):
     '''
     Draws all lines from a HoughLineP transform onto a source image
@@ -39,90 +41,195 @@ def draw_lines(src, lines):
     :param lines: 
     :return: 
     '''
-    try:
-        for line in lines:
-            for x1, y1, x2, y2 in line:
-                cv.line(src, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    except TypeError:
-        print "No lines detected"
+    print len(lines[0])
+    if lines is None or len(lines) == 0:
+        return
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv.line(src, (x1, y1), (x2, y2), (0, 255, 0), 4)
 
 
-print("open webcam")
-# webcam = cv.VideoCapture(1)
-detector = cv.SimpleBlobDetector()
+def draw_point(src, coord):
+    """
+    Draws a circular point centered of coord
+    :param src: Source image to draw on
+    :param coord: coordinate to point towards as a tuple
+    :return:
+    """
+    cv.circle(src, coord, 4, (0, 255, 0))
 
-while True:
-    # ret, frame = webcam.read()
-    frame = cv.imread("chess2.jpg")
-    size = frame.shape
+
+def grab_frame(webcam_source):
+    """
+    Grabs and returns a single frame from a webcam source
+    """
+    ret, f = webcam_source.read()
+    size = f.shape
 
     # if the image is too large, resize it
-    if size[0] * size[1] > 10**6:
-        frame = cv.resize(frame, (0,0), fx=0.1, fy=0.1)
+    if size[0] * size[1] > 10 ** 6:
+        f = cv.resize(f, (0, 0), fx=0.1, fy=0.1)
+    return f
 
-    grayscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    grayscale = cv.blur(grayscale, (3,3))
-    threshold = cv.adaptiveThreshold(grayscale, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+
+def image_threshold(image_matrix):
+    '''
+    Takes in a numPY matrix as a image and returns the threshold of that image
+    :return:
+    '''
+    grayscale = cv.cvtColor(image_matrix, cv.COLOR_BGR2GRAY)
+    grayscale = cv.blur(grayscale, (2,2))
+    threshold = cv.adaptiveThreshold(grayscale, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 3)
     threshold = cv.bitwise_not(threshold)
+    return threshold
 
-    # houghlines = cv.HoughLinesP(threshold, 1, 3.14159, 200, minLineLength=100, maxLineGap=50)
+
+def find_center(contour):
+    """
+    Finds the centerpoint of a closed loop contour
+    :param contour:
+    :return: (x,y) corrdinates of the centerpoint
+    """
+    moments = cv.moments(contour)
+    cX = int(moments["m10"] / moments["m00"])
+    cY = int(moments["m01"] / moments["m00"])
+    return cX, cY
+
+
+def get_contours(image_matrix):
+    '''
+    Gets the contours in an image matrix
+    :return:
+    '''
     # Detect the board (Should have a white outline)
-    contours, hier = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    contours, hier = cv.findContours(image_matrix, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    return contours
 
 
-    # find largest object with four corners
-    area = cv.contourArea(contours[0])
-    cont = contours[0]
-    for contour in contours:
-        loc = cv.contourArea(contour)
-        approx = cv.approxPolyDP(contour, 0.1 * cv.arcLength(contour, True), True)
+def sort_contours(contours):
+    """
+    Sorts a set of contours from smallest to largest
+    :param contours:
+    :return:
+    """
+    contours_sorted = sorted(contours, key=lambda x: cv.contourArea(x))
+    return contours_sorted
 
-        # Find the largest square contour
-        if loc > area and len(approx) == 4:
-            area = cv.contourArea(contour)
-            cont = contour
 
-    # Create a mask for the largest 4 sided contour
+def get_approx_sides(contour):
+    """
+    Gets the approximate number of sides a contour shape has
+    assumes that the shape is closed
+    :param contour:
+    :return:
+    """
+    approx = cv.approxPolyDP(contour, 0.5 * cv.arcLength(contour, True), True)
+    return len(approx)
+
+
+def get_largest_square(contours):
+    """
+    Gets the largest 4 sided contour in a set
+    Used to detect a game board
+    :param contours: set of contours as returned by cv.findContours
+    :return: Largest 4 sided shape
+    """
+    cnts_sorted = sort_contours(contours)
+    arr_len = len(cnts_sorted) - 1
+    for x in range(arr_len, 0):
+        contour = cnts_sorted[x]
+        sides = get_approx_sides(contour)
+        if sides == 4:
+            return contour
+
+    # Return the largest contour if no rectangles have been found as a failsafe
+    return cnts_sorted[arr_len]
+
+
+def contour_to_mask(contour, frame):
+    """
+    Takes a contour, fills it, and then creates an image mask out of it
+    :param contour: Contour to create a mask from
+    :param frame: Frame to reference when created the mask
+    :return:
+    """
+    # First create an empty mask with the width and height of the image frame
     mask = np.zeros(frame.shape, dtype='uint8')
     mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
-    cv.drawContours(mask, [cont], -1, (255, 255, 255), thickness=cv.FILLED)
-
-    cv.drawContours(frame, [cont], -1, (0, 0, 255), thickness=3)
-
+    cv.drawContours(mask, [contour], -1, (255, 255, 255), thickness=cv.FILLED)
+    cv.drawContours(frame, [contour], -1, (0, 0, 255), thickness=3)
     frame_masked = cv.bitwise_and(frame, frame, mask=mask)
+    return frame_masked
 
-    # perform hough lines on the masked image to find grid
-    frameMaskedGray = cv.cvtColor(frame_masked, cv.COLOR_BGR2GRAY)
-    edges = cv.Canny(frameMaskedGray, 100, 150, apertureSize=3)
 
-    lines = cv.HoughLinesP(edges, 1, 3.14159/180, 50, minLineLength=0, maxLineGap=100)
-    draw_lines(frame, lines)
+def quick_canny(image):
+    """
+    Quicky convert frame to black and white and then edge detect
+    :param image:
+    :return:
+    """
+    bw = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    canny = cv.Canny(bw, 50, 150)
+    return canny
 
-    # Create a new image using the four corners of the detected chess board
-    # Start by remaking the contour mask
-    mask = cv.Canny(mask,50,255)
-    lines = cv.HoughLinesP(mask, 1, 3.14/720, 10, minLineLength=5, maxLineGap=300)
-    draw_lines(frame_masked, lines)
 
-    # Create our numpy array for storing points in an image
-    point_coords = []
-    for i in range(1, len(lines)):
-        intersect, constr = intersection(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3], lines[i-1][0][0], lines[i-1][0][1], lines[i-1][0][2], lines[i-1][0][3])
-        if intersect == 0:
-            continue
-        else:
-            point_coords.append(intersect)
+def get_lines(image):
+    """
+    Performs a HoughLineP transform on the specified image
+    :param image:
+    :return:
+    """
+    canny = quick_canny(image)
+    cv.imshow("canny", canny)
+    lines = cv.HoughLinesP(canny, 1, 3.14159 / 180, 50,  minLineLength=100, maxLineGap=100)
+    return lines
 
-    for coord in point_coords:
-        cv.circle(frame,coord,3,(255,0,0),-1)
 
-    cv.imshow("press q to quit", frame_masked)
-    cv.imshow("b", frame)
-    cv.imshow("a", mask)
-    cv.imshow('c', edges)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+def find_grid(image):
+    """
+    Method to find a grid on an image
+    normally takes in a masked image of a chess board
+    :param image: numpy matrix with image data
+    :return: coordinates of all intersections in image
+    """
+    # Find all lines in the image
+    lines = get_lines(image)
 
-# webcam.release()
-cv.destroyAllWindows()
 
+def find_chess_board(image_source):
+    """
+    Finds the contour for the chessboard in an image
+    assumes the chessboard is the largest square object in the frame
+    :param image_source:
+    :return:
+    """
+    thresh = image_threshold(image_source)
+
+    # Find all the contours in the image and then
+    # find largest object with four corners
+    conts = get_contours(thresh)
+    square_contour = get_largest_square(conts)
+    return square_contour
+
+
+def main():
+    print("open webcam")
+    webcam = cv.VideoCapture(0)
+    while True:
+        frame = grab_frame(webcam)
+
+        chess_board = find_chess_board(frame)
+        # Create a mask for the largest 4 sided contour
+        masked = contour_to_mask(chess_board, frame)
+
+        center_point = find_center(chess_board)
+        draw_point(frame, center_point)
+        cv.imshow("press q to quit", masked)
+        cv.imshow("camera feed", frame)
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            break
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
